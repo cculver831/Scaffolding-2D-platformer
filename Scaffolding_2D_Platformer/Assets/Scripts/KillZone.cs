055b01f2ee67470085830594f4da21db339db096
CCulver831 <c.culver831@gmail.com>
2020-01-21 19:08:10 -0800

    Added a respawn and code completely commented

---
+++ Scaffolding_2D_Platformer/Assets/Scripts/KillZone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+    //This code is attached to a prefab called killzone, when the player hits the killzone, he/she will be reset to the checkpoint created
+
+    //references to checkpoint
+    [SerializeField]
+    public GameObject CheckPoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Vector3 temp = CheckPoint.transform.position;
+            collision.transform.position = temp;
+        }
+    }
+}
